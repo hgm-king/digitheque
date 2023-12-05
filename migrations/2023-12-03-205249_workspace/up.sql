b95f19536fd26d754ff5d6ce314880a2104ef25b
hgm-king <hgmaxwellking@gmail.com>
2023-12-05 13:44:15 -0500

    Fix password in migration

--- migrations/2023-12-03-205249_workspace/up.sql
+++ migrations/2023-12-03-205249_workspace/up.sql
@@ -2,7 +2,7 @@
 CREATE TABLE "user" (
     id SERIAL PRIMARY KEY,
     username VARCHAR(48) NOT NULL,
-    password varchar(48) NOT NULL,
+    password varchar(64) NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP,
     deleted_at TIMESTAMP,
